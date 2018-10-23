5c709f79d73fcb3b1f551d8d761e1923819432d5
Kevin Tumbo <kevintumbo@Kevins-MacBook-Pro-2.local>
2018-10-23 21:26:29 +0300

    changes

--- android/app/src/main/java/com/autismapp/MainApplication.java
+++ android/app/src/main/java/com/autismapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.autismapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new SQLitePluginPackage(),
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNFetchBlobPackage(),
+            new VectorIconsPackage()
       );
     }
 
